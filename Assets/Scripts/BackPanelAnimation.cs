36744006e7093704d00d3dbf6e3d268ef4c2af19
Van DeSantis <vandesantis98@gmail.com>
2019-12-01 21:54:28 -0600

    Add to backpanel animation

--- Assets/Scripts/BackPanelAnimation.cs
+++ Assets/Scripts/BackPanelAnimation.cs
@@ -18,22 +18,27 @@ public class BackPanelAnimation : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.T))
         {
-            BackPanel();
+            StartCoroutine(BackPanel());
         }
     }
 
-    //private IEnumerator BackPanel()
-    //{
-    //    anim.Play("BackPanel");
-    //    //anim.SetBool("Audio", true);
-    //    //GetComponent<AudioSource>().Play();
-    //    //yield return new WaitForSeconds(GetComponent<AudioSource>().clip.length);
-    //    //anim.SetBool("Audio", false);
-    //}
-
-    private void BackPanel()
+    private IEnumerator BackPanel()
     {
         anim.Play("AvatarBackpanel");
+        yield return new WaitForSeconds(anim.GetCurrentAnimatorStateInfo(0).length);
         backpanel.transform.parent = this.transform;
+        anim.Play("AvatarBackpanel2");
+
+        //anim.SetBool("Audio", true);
+        //GetComponent<AudioSource>().Play();
+        //yield return new WaitForSeconds(GetComponent<AudioSource>().clip.length);
+        //anim.SetBool("Audio", false);
     }
+
+    //private void BackPanel()
+    //{
+    //    anim.Play("AvatarBackpanel");
+    //    backpanel.transform.parent = this.transform;
+    //    anim.Play("AvatarBackpanel2");
+    //}
 }
