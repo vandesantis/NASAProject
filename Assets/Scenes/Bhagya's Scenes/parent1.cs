8681ae78ea0241558d59b38bdb6c730fa12e163b
Bhagya Hosur <bhagyahosur77@gmail.com>
2019-12-04 16:19:40 -0600

    Battery Animation

---
+++ Assets/Scenes/Bhagya's Scenes/parent1.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class parent : MonoBehaviour
+{
+	public GameObject q; //arm;
+	public GameObject w; //battery;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	
+	void OnCollisionEnter(Collision otherObj)
+	{
+		if(otherObj.gameObject == GameObject.Find("w"))
+		{
+			w.transform.parent = GameObject.Find("q").transform;
+		}
+	}
+}
