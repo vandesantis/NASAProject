ec8bb9b489cbe2977a96d877f1936bba329e61eb
Vanimal <vandesantis98@gmail.com>
2019-12-06 14:49:38 -0600

    Add text to hololens scene

--- Assets/Scripts/NewProjectController.cs
+++ Assets/Scripts/NewProjectController.cs
@@ -1,14 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NewProjectController : MonoBehaviour
 {
     private int currentTaskNum;
     private int numTasks = 8;
 
-    //public Text taskText;
-    //private string[] taskStrings;
+    public Text taskText;
+    private string[] taskStrings;
     //private bool prev;
 
     private Animator anim;
@@ -17,15 +18,15 @@ public class NewProjectController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //taskStrings = new string[numTasks];
-        //taskStrings[0] = "Task 1: Remove battery";
-        //taskStrings[1] = "Task 2: Unscrew back panel";
-        //taskStrings[2] = "Task 3: Remove back panel";
-        //taskStrings[3] = "Task 4: Remove ram from slot";
-        //taskStrings[4] = "Task 5: Insert new ram";
-        //taskStrings[5] = "Task 6: Put on back panel";
-        //taskStrings[6] = "Task 7: Screw in back panel";
-        //taskStrings[7] = "Task 7: Replace battery";
+        taskStrings = new string[numTasks];
+        taskStrings[0] = "Task 1: Remove battery";
+        taskStrings[1] = "Task 2: Unscrew back panel";
+        taskStrings[2] = "Task 3: Remove back panel";
+        taskStrings[3] = "Task 4: Remove ram from slot";
+        taskStrings[4] = "Task 5: Insert new ram";
+        taskStrings[5] = "Task 6: Put on back panel";
+        taskStrings[6] = "Task 7: Screw in back panel";
+        taskStrings[7] = "Task 7: Replace battery";
         //taskStrings[8] = "Task 8";
         //taskStrings[9] = "Task 9";
         //taskStrings[10] = "Task 10";
@@ -34,7 +35,7 @@ public class NewProjectController : MonoBehaviour
         anim = GetComponent<Animator>();
         cam = GetComponent<Animator>();
         currentTaskNum = 0;
-        //taskText.text = taskStrings[currentTaskNum];
+        taskText.text = taskStrings[currentTaskNum];
     }
 
     // Update is called once per frame
@@ -53,71 +54,73 @@ public class NewProjectController : MonoBehaviour
 
     public void NextTask()
     {
-            switch (currentTaskNum)
-            {
-                case 0:
-                    Walk();
-                    break;
-                case 1:
-                    Battery();
-                    break;
-                case 2:
-                    Screws();
-                    break;
-                case 3:
-                    Backpanel();
-                    break;
-                case 4:
-                    RamInsertion();
-                    break;
-                case 5:
-                    BackpanelRev();
+        taskText.text = taskStrings[currentTaskNum];
+        switch (currentTaskNum)
+        {
+            case 0:
+                Walk();
+                break;
+            case 1:
+                Battery();
+                break;
+            case 2:
+                Screws();
+                break;
+            case 3:
+                Backpanel();
+                break;
+            case 4:
+                RamInsertion();
+                break;
+            case 5:
+                BackpanelRev();
 
-                    break;
-                case 6:
-                    ScrewsRev();
-                    break;
-                case 7:
-                    BatteryRev();
+                break;
+            case 6:
+                ScrewsRev();
+                break;
+            case 7:
+                BatteryRev();
 
-                    break;
-            }
-            currentTaskNum++;
+                break;
+        }
+        currentTaskNum++;
     
     }
 
     public void PrevTask()
     {
-            switch (currentTaskNum)
-            {
-                case 0:
-                    break;
-                case 1:
-                    WalkRestart();
-                    break;
-                case 2:
-                    BatteryRestart();
-                    break;
-                case 3:
-                    ScrewsRestart();
-                    break;
-                case 4:
-                    BackpanelRestart();
-                    break;
-                case 5:
-                    RamInsertionRestart();
-                    break;
-                case 6:
-                    BackpanelRevRestart();
-                    break;
-                case 7:
-                    ScrewsRevRestart();
-                    break;
-                case 8:
-                    BatteryRevStart();
-                    break;
-            }
-            currentTaskNum--;
+        taskText.text = taskStrings[currentTaskNum];
+        switch (currentTaskNum)
+        {
+            case 0:
+                break;
+            case 1:
+                WalkRestart();
+                break;
+            case 2:
+                BatteryRestart();
+                break;
+            case 3:
+                ScrewsRestart();
+                break;
+            case 4:
+                BackpanelRestart();
+                break;
+            case 5:
+                RamInsertionRestart();
+                break;
+            case 6:
+                BackpanelRevRestart();
+                break;
+            case 7:
+                ScrewsRevRestart();
+                break;
+            case 8:
+                BatteryRevStart();
+                break;
+        }
+        currentTaskNum--;
         
     }
 
